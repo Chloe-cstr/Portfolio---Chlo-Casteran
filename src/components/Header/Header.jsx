f670bfd4cd737585c307ab634383e4cc2da19d31
Chloe Casteran <chloe.casteran@gmail.com>
2025-04-17 15:43:00 +0200

    Initialisation du projet

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/images/logo.png';
+import './header.scss'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={Logo} alt="Logo Kasa" className="header__logo" />
+      <nav className="header__nav">
+        <ul className="header__nav__list">
+          <li className="header__nav__item">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "header__nav__item__maj active" : "header__nav__item__maj"
+            }
+          >
+            Accueil
+          </NavLink>
+          </li>
+          <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "header__nav__item__maj active" : "header__nav__item__maj"
+            }
+          >
+            A propos
+          </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header/Header';
+
+
+const Home = () => {
+  return (
+    <div className="body-container">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
